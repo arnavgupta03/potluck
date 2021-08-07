02ad3a5d1cbce6b3f2138a27b22e24454aba0da8
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2021-08-06 21:19:36 -0400

    Add react boilerplate

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -3,7 +3,7 @@ import '../styles/landing.css'
 function Home() {
   return (
     <div className="App">
-      
+      This is the homepage.
     </div>
   );
 }
---
+++ frontend/src/pages/login.js
@@ -0,0 +1,8 @@
+import React from "react";
+import '../styles/loginRegister.css';
+
+function Login() {
+
+}
+
+export default Login;
