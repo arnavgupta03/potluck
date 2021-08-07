ec83c798ab6ff1844db705dcf4b266c037870c51
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2021-08-06 20:09:34 -0400

    Add logo and link for header

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,4 +1,4 @@
-import logo from '../logo.svg';
+import logo from '../logo.png';
 import '../styles/landing.css'
 
 function Home() {
@@ -6,16 +6,11 @@ function Home() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          href="/"
         >
-          Learn React
+          Potluck
         </a>
       </header>
     </div>
