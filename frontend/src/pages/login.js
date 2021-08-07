c8bea015904635abfbc24a11981226a79dd101de
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2021-08-07 13:31:51 -0400

    Fix login and profile setup

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -4,10 +4,17 @@ import '../styles/loginRegister.css';
 
 function Login() {
     const [location, setLocation] = useLocation();
+
+    const doTheRegister = () => {
+        sessionStorage.setItem("username", document.getElementById("username").value);
+        sessionStorage.setItem("password", document.getElementById("password").value);
+        setLocation("/setupProfile");
+    }
+
     return (
         <div>
-            <h1 className="display-1 mx-1">Register</h1>
-            <form action="http://localhost:5000/loginUser" method="post" target="dummyframe">
+            <h1 className="display-1 mx-1">Log In</h1>
+            <form id="form">{/* action="http://localhost:5000/loginUser" method="post" target="dummyframe">*/}
                 <div className="form-group">
                     <label for="username" className="mx-2">Username</label><br />
                     <input type="text" id="username" name="username" placeholder="Enter username" className="m-2" />
@@ -16,8 +23,8 @@ function Login() {
                     <label for="password" className="mx-2">Password</label><br />
                     <input type="password" name="password" id="password" placeholder="Enter password" className="m-2" />
                 </div>
-                <button onClick={() => setLocation("/feed")} type="submit" className="btn btn-primary m-2">Submit</button>
             </form>
+            <button onClick={doTheRegister} className="btn btn-primary m-2">Submit</button>
             <iframe name="dummyframe" id="dummyframe" style={{visibility: 'hidden'}}></iframe>
         </div>
     );
