9346401ce3e20ccba44f5c2c0ed133eab35150d1
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2021-08-06 20:24:44 -0400

    Add basic navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './styles/App.css';
 import React, { useState, useEffect } from "react";
 import { Router, Link } from "wouter";
@@ -8,18 +8,21 @@ import useHashLocation from "./hooks/wouter-hash";
 export default function App() {
   return (
     <Router hook={useHashLocation}>
+      <nav className="navbar navbar-light bg-light">
+        <Link
+          className="navbar-brand"
+          href="/"
+        >
+          <img src={logo} className="img-fluid m-2" alt="logo" width="75"/>
+          Potluck
+        </Link>
+        <Link href="/about">About</Link>
+      </nav>
       <main role="main" className="wrapper">
         <div className="content">
           {/* Router specifies which component to insert here as the main content */}
           <PageRouter />
         </div>
-        <footer className="footer">
-        <div className="links">
-          <Link href="/">Home</Link>
-          <span className="divider">|</span>
-          <Link href="/about">About</Link>
-        </div>
-      </footer>
       </main>
     </Router>
   );
--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -4,15 +4,7 @@ import '../styles/landing.css'
 function Home() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <a
-          className="App-link"
-          href="/"
-        >
-          Potluck
-        </a>
-      </header>
+      
     </div>
   );
 }
