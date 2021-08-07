a23801fc1492c8a689debdc004be1c1fb3a749c4
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2021-08-07 00:40:29 -0400

    Add form to login

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -1,8 +1,26 @@
 import React from "react";
+import { Router, Link, useLocation } from "wouter";
 import '../styles/loginRegister.css';
 
 function Login() {
-
+    const [location, setLocation] = useLocation();
+    return (
+        <div>
+            <h1 className="display-1 mx-1">Register</h1>
+            <form action="http://localhost:5000/loginUser" method="post" target="dummyframe">
+                <div className="form-group">
+                    <label for="username" className="mx-2">Username</label><br />
+                    <input type="text" id="username" name="username" placeholder="Enter username" className="m-2" />
+                </div>
+                <div className="form-group">
+                    <label for="password" className="mx-2">Password</label><br />
+                    <input type="password" name="password" id="password" placeholder="Enter password" className="m-2" />
+                </div>
+                <button onClick={() => setLocation("/feed")} type="submit" className="btn btn-primary m-2">Submit</button>
+            </form>
+            <iframe name="dummyframe" id="dummyframe" style={{visibility: 'hidden'}}></iframe>
+        </div>
+    );
 }
 
 export default Login;
