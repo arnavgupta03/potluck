5b480221ab59267885546a76b8dd86b7410cc909
harshulgupta05 <65627692+harshulgupta05@users.noreply.github.com>
2021-08-07 11:50:53 -0400

    fixed navbar and register page

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -51,23 +51,23 @@ function Register() {
     }
 
     return (
-        <div>
-            <h1 className="display-1 mx-1">Register</h1>
+        <div className="register">
+            <h1 className="display-1 mx-1 text-center">Register</h1>
             <form action="http://localhost:5000/registerUser" method="post" target="dummyframe">
-                <div className="form-group">
+                <div className="form-group text-center">
                     <label for="username" className="mx-2">Username</label><br />
                     <input type="text" id="username" name="username" placeholder="Enter username" className="m-2" />
                 </div>
-                <div className="form-group">
+                <div className="form-group text-center">
                     <label for="password" className="mx-2">Password</label><br />
                     <input type="password" name="password" id="password" placeholder="Enter password" className="m-2" /> <br />
                     <small id="passwordTip" className="form-text text-muted m-2">Make sure it's something unique.</small>
                 </div>
-                <div className="form-group">
+                <div className="form-group text-center">
                     <label for="name" className="mx-2">Name</label><br />
                     <input type="text" name="name" id="name" placeholder="Enter name" className="m-2" />
                 </div>
-                <div className="form-group">
+                <div className="form-group text-center">
                     <label for="bio" className="mx-2">Bio</label><br />
                     <input type="text" name="bio" id="bio" placeholder="Enter a simple bio" className="m-2" />
                 </div>
@@ -75,7 +75,9 @@ function Register() {
                     <label for="file" className="mx-2">Profile Photo</label><br />
                     <input type="file" name="profilepic" accept="image/*" id="profilepic" className="m-2" />
                 </div>*/}
-                <button onClick={doTheRegister} type="submit" className="btn btn-primary m-2">Submit</button>
+                <div classname="text-center">
+                    <button onClick={doTheRegister} type="submit" className="btn btn-primary m-2 text-center">Submit</button>
+                </div>
             </form>
             <iframe name="dummyframe" id="dummyframe" style={{visibility: 'hidden'}}></iframe>
         </div>
