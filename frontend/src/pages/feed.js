82d7b3f6186a4355aaf927eea6dbaea6e1dba8a8
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2021-08-07 00:39:25 -0400

    Add feed boilerplate code

---
+++ frontend/src/pages/feed.js
@@ -0,0 +1,12 @@
+import React from "react"
+import '../styles/feed.css';
+
+function Feed() {
+    return(
+        <div>
+            This is the feed page.
+        </div>
+    )
+}
+
+export default Feed;
