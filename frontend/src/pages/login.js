6732dd3915a38597059a040a085b4a9001af46a8
harshulgupta05 <65627692+harshulgupta05@users.noreply.github.com>
2021-08-07 15:27:32 -0400

    centered login page

--- frontend/src/pages/login.js
+++ frontend/src/pages/login.js
@@ -6,17 +6,19 @@ function Login() {
     const [location, setLocation] = useLocation();
     return (
         <div>
-            <h1 className="display-1 mx-1">Register</h1>
+            <h1 className="display-1 mx-1 text-center">Login</h1>
             <form action="http://localhost:5000/loginUser" method="post" target="dummyframe">
-                <div className="form-group">
+                <div className="form-group text-center">
                     <label for="username" className="mx-2">Username</label><br />
                     <input type="text" id="username" name="username" placeholder="Enter username" className="m-2" />
                 </div>
-                <div className="form-group">
+                <div className="form-group text-center">
                     <label for="password" className="mx-2">Password</label><br />
                     <input type="password" name="password" id="password" placeholder="Enter password" className="m-2" />
                 </div>
-                <button onClick={() => setLocation("/feed")} type="submit" className="btn btn-primary m-2">Submit</button>
+                <div className="text-center">
+                    <button onClick={() => setLocation("/feed")} type="submit" className="btn btn-primary m-2">Submit</button>
+                </div>
             </form>
             <iframe name="dummyframe" id="dummyframe" style={{visibility: 'hidden'}}></iframe>
         </div>
