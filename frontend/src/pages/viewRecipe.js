641581a868df901a7c602e6449907ceb02a2f7db
Arnav <65367548+arnavgupta03@users.noreply.github.com>
2021-08-07 21:49:32 -0400

    Add boilerplate code for view recipe page

---
+++ frontend/src/pages/viewRecipe.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+class ViewRecipe extends React.Component {
+    render() {
+        return (
+            <div>
+                This is the view recipe page.
+            </div>
+        );
+    }
+}
+
+export default ViewRecipe;
