d79495422bd40436ec83d0d282b6ccb45a96eb9f
harshulgupta05 <65627692+harshulgupta05@users.noreply.github.com>
2021-08-07 21:53:51 -0400

    styling for addRecipe

--- frontend/src/pages/addRecipe.js
+++ frontend/src/pages/addRecipe.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react"
 import { Router, Link, useLocation } from "wouter";
+import logo from "../logo.png"
 
 function AddRecipe() {
     const [count, setCount] = useState(1);
@@ -59,25 +60,53 @@ function AddRecipe() {
 
     return(
         <div>
-            <h3 className="display-3 text-center">Add a Recipe</h3>
-            <div className="form-group text-center">
-                <label for="recipename" className="mx-2">Recipe Name</label><br />
-                <input type="text" id="recipename" name="recipename" placeholder="What'd you make?" className="m-2" />
-            </div>
-            <div className="form-group text-center">
-                <label for="recipesteps" className="mx-2">Steps (no commas)</label><br />
-                {[...Array(count)].map((value, index) => (<div><input type="text" id={"recipesteps" + index} name="recipesteps" placeholder="How'd you make it?" className="m-2" /><br /></div>))}
-            </div>
-            <div className="text-center">
-                <button onClick={addLine} type="submit" className="btn btn-primary m-2 text-center">Add Step</button>
-            </div>
-            <div className="form-group text-center">
-                <label for="profilepic" className="mx-2">Recipe Photo</label><br />
-                {/*<input type="file" name="profilepic" accept="image/*" id="profilepic" className="m-2" />*/}
-                <button onClick={showWidget} className="btn btn-primary m-2 text-center">Upload Photo</button>
-            </div>
-            <div className="text-center">
-                <button onClick={doTheAdd} type="submit" className="btn btn-primary m-2 text-center">Submit</button>
+            <nav className="navbar navbar-expand-lg navbar-light bg-light">
+                <div className="container-fluid">
+                    <Link
+                        className="navbar-brand"
+                        href="/"
+                    >
+                        <img src={logo} className="img-fluid m-2" alt="logo" width="36"/>
+                        Potluck
+                    </Link>
+                    <div className="collapse navbar-collapse">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item pt-auto pb-auto pl-2">
+                                <Link href="/about" className="nav-link active pt-auto pb-auto pl-2">About</Link>
+                            </li>
+                            <li className="nav-item pt-auto pb-auto pl-2">
+                                <Link href="/feed" className="nav-link active pt-auto pb-auto pl-2">My Feed</Link>
+                            </li>
+                            <li className="nav-item pt-auto pb-auto pl-2">
+                                <Link href="/addRecipe" className="nav-link active pt-auto pb-auto pl-2">New Recipe</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div className="w-100 text-center ml-auto mr-auto d-flex justify-content-center pt-5 align-items-center">
+                <div className="register shadow">
+                    <h3 className="display-3 text-center">Add a Recipe</h3>
+                    <div className="form-group text-center">
+                        <label for="recipename" className="mx-2 form-label">Recipe Name</label><br />
+                        <input type="text" id="recipename" name="recipename" placeholder="What'd you make?" className="m-auto pl-3 form-control w-75" />
+                    </div>
+                    <div className="form-group text-center">
+                        <label for="recipesteps" className="mx-2 form-label">Steps (no commas)</label><br />
+                        {[...Array(count)].map((value, index) => (<div><input type="text" id={"recipesteps" + index} name="recipesteps" placeholder="How'd you make it?" className="m-auto pl-3 form-control w-75" /><br /></div>))}
+                    </div>
+                    <div className="text-center">
+                        <button onClick={addLine} type="submit" className="btn btn-primary m-2 text-center">Add Step</button>
+                    </div>
+                    <div className="form-group text-center">
+                        <label for="profilepic" className="mx-2 form-label">Recipe Photo</label><br />
+                        {/*<input type="file" name="profilepic" accept="image/*" id="profilepic" className="m-2" />*/}
+                        <button onClick={showWidget} className="btn btn-primary m-2 text-center">Upload Photo</button>
+                    </div>
+                    <div className="text-center">
+                        <button onClick={doTheAdd} type="submit" className="btn btn-primary m-2 text-center">Submit</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
