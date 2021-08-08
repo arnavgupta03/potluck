241543f703ae5420abe712cb07032d999db31671
harshulgupta05 <65627692+harshulgupta05@users.noreply.github.com>
2021-08-07 20:33:12 -0400

    styled register page

--- frontend/src/pages/register.js
+++ frontend/src/pages/register.js
@@ -53,7 +53,7 @@ function Register() {
     }
 
     return (
-        <div className="register">
+        <div>
             <nav className="navbar navbar-expand-lg navbar-light bg-light">
                 <div className="container-fluid">
                     <Link
@@ -78,33 +78,37 @@ function Register() {
                     </div>
                 </div>
             </nav>
-            <h1 className="display-1 mx-1 text-center">Register</h1>
-            <form action="http://localhost:5000/registerUser" method="post" target="dummyframe">
-                <div className="form-group text-center">
-                    <label for="username" className="mx-2">Username</label><br />
-                    <input type="text" id="username" name="username" placeholder="Enter username" className="m-2" />
+            <div className="w-100 text-center ml-auto mr-auto d-flex justify-content-center pt-5 align-items-center">
+                <div className="register shadow">
+                    <h1 className="display-1 mx-1 text-center pb-2">Register</h1>
+                    <form action="http://localhost:5000/registerUser" method="post" target="dummyframe">
+                        <div className="form-group text-center pb-3">
+                            <label for="username" className="mx-2 form-label">Username</label><br />
+                            <input type="text" id="username" name="username" placeholder="Enter username" className="m-auto pl-3 form-control w-75" />
+                        </div>
+                        <div className="form-group text-center">
+                            <label for="password" className="mx-2 form-label">Password</label><br />
+                            <input type="password" name="password" id="password" placeholder="Enter password" className="m-auto pl-3 form-control w-75" /> <br />
+                            <small id="passwordTip" className="form-text text-muted">Make sure it's something unique.</small>
+                        </div>
+                        <div className="form-group text-center">
+                            <label for="name" className="mx-2 form-label">Name</label><br />
+                            <input type="text" name="name" id="name" placeholder="Enter name" className="m-auto pl-3 form-control w-75" />
+                        </div>
+                        <div className="form-group text-center">
+                            <label for="bio" className="mx-2 form-label">Bio</label><br />
+                            <input type="text" name="bio" id="bio" placeholder="Enter a simple bio" className="m-auto pl-3 form-control w-75" />
+                        </div>
+                        {/*<div className="form-group">
+                            <label for="file" className="mx-2">Profile Photo</label><br />
+                            <input type="file" name="profilepic" accept="image/*" id="profilepic" className="m-2" />
+                        </div>*/}
+                        <div className="text-center">
+                            <button onClick={doTheRegister} type="submit" className="btn btn-primary m-2 text-center">Submit</button>
+                        </div>
+                    </form>
                 </div>
-                <div className="form-group text-center">
-                    <label for="password" className="mx-2">Password</label><br />
-                    <input type="password" name="password" id="password" placeholder="Enter password" className="m-2" /> <br />
-                    <small id="passwordTip" className="form-text text-muted m-2">Make sure it's something unique.</small>
-                </div>
-                <div className="form-group text-center">
-                    <label for="name" className="mx-2">Name</label><br />
-                    <input type="text" name="name" id="name" placeholder="Enter name" className="m-2" />
-                </div>
-                <div className="form-group text-center">
-                    <label for="bio" className="mx-2">Bio</label><br />
-                    <input type="text" name="bio" id="bio" placeholder="Enter a simple bio" className="m-2" />
-                </div>
-                {/*<div className="form-group">
-                    <label for="file" className="mx-2">Profile Photo</label><br />
-                    <input type="file" name="profilepic" accept="image/*" id="profilepic" className="m-2" />
-                </div>*/}
-                <div className="text-center">
-                    <button onClick={doTheRegister} type="submit" className="btn btn-primary m-2 text-center">Submit</button>
-                </div>
-            </form>
+            </div>            
             <iframe name="dummyframe" id="dummyframe" style={{visibility: 'hidden'}}></iframe>
         </div>
     );
