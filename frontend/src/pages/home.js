6ae704b09227e922209cf3b178962832e8c3e0bc
harshulgupta05 <65627692+harshulgupta05@users.noreply.github.com>
2021-08-08 02:13:15 -0400

    added homepage styling

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,35 +1,41 @@
 import '../styles/landing.css'
-import logo from "../logo.png"
+import logo from "../logo.png";
+import home from "../home.jpg";
 import { Router, Link } from "wouter";
 
 function Home() {
   return (
     <div className="App">
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <div className="container-fluid">
-          <Link
-            className="navbar-brand"
-            href="/"
-          >
-            <img src={logo} className="img-fluid m-2" alt="logo" width="36"/>
-            Potluck
-          </Link>
-          <div className="collapse navbar-collapse">
-            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-              <li className="nav-item pt-auto pb-auto pl-2">
-                <Link href="/about" className="nav-link active pt-auto pb-auto pl-2">About</Link>
-              </li>
-              <li className="nav-item pt-auto pb-auto pl-2">
-                <Link href="/register" className="nav-link active pt-auto pb-auto pl-2">Register</Link>
-              </li>
-              <li className="nav-item pt-auto pb-auto pl-2">
-                <Link href="/login" className="nav-link active pt-auto pb-auto pl-2">Login</Link>
-              </li>
-            </ul>
+      <div className="fadein homepage">
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+          <div className="container-fluid">
+            <Link
+              className="navbar-brand"
+              href="/"
+            >
+              <img src={logo} className="img-fluid m-2" alt="logo" width="36"/>
+              Potluck
+            </Link>
+            <div className="collapse navbar-collapse">
+              <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <li className="nav-item pt-auto pb-auto pl-2">
+                  <Link href="/about" className="nav-link active pt-auto pb-auto pl-2">About</Link>
+                </li>
+                <li className="nav-item pt-auto pb-auto pl-2">
+                  <Link href="/register" className="nav-link active pt-auto pb-auto pl-2">Register</Link>
+                </li>
+                <li className="nav-item pt-auto pb-auto pl-2">
+                  <Link href="/login" className="nav-link active pt-auto pb-auto pl-2">Login</Link>
+                </li>
+              </ul>
+            </div>
           </div>
+        </nav>
+        <div className="imgDiv">
+          <img src={home} className="homeimg" alt="'Potluck Cooking' by The Tedster is licensed with CC BY-SA 2.0. To view a copy of this license, visit https://creativecommons.org/licenses/by-sa/2.0/"></img>
         </div>
-      </nav>
-      This is the homepage.
+        <h2 className="text-center desc">Potluck. The social media app for food.</h2>
+      </div>
     </div>
   );
 }
